4adddeeb5a54aa7d6714eba9fe88bd901c0c8c79
developerTajul <developer.tajul@gmail.com>
2022-01-23 15:07:06 +0600

    populating items

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
 const filterInput = document.querySelector('#filter');
-const productsList = document.querySelector('.collection');
+const productsListUL = document.querySelector('.collection');
 const productName = document.querySelector('.product-name');
 const productPrice = document.querySelector('.product-price');
 const productAdd = document.querySelector('.add-product');
@@ -13,15 +13,32 @@ const productData = [
         price: 20 
     },
     {
-        id: 0,
-        name: 'Alu',
-        price: 20 
+        id: 1,
+        name: 'Potol',
+        price: 30 
     },
     {
-        id: 0,
-        name: 'Alu',
-        price: 20 
+        id: 2,
+        name: 'Begun',
+        price: 60 
     }
 ]
 
-console.log( productData );
+function getData( productsList ){
+    let li = "";
+    productsList.forEach( (product) => {
+        li = document.createElement('li');
+        li.className = 'list-group-item collection-item';
+        li.id = 'product-'+ product.id;
+        li.innerHTML = `
+        <strong class="product-name">${product.name}</strong>
+		<span class="product-price">$${product.price}</span>
+		<i class="far fa-trash-alt float-end delete-product"></i>
+        `;
+        productsListUL.appendChild(li);
+    });
+
+    
+}
+
+getData(productData);
