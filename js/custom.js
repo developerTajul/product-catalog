31117aaef4efdbd22fb56495c3bd9e3d39e146bb
developerTajul <developer.tajul@gmail.com>
2022-01-23 13:01:00 +0600

    item selected by querySelector

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,27 @@
-console.log("connected successfully");
+const filterInput = document.querySelector('#filter');
+const productsList = document.querySelector('.collection');
+const productName = document.querySelector('.product-name');
+const productPrice = document.querySelector('.product-price');
+const productAdd = document.querySelector('.add-product');
+const productdelete = document.querySelector('.delete-product');
+
+// data / state
+const productData = [
+    {
+        id: 0,
+        name: 'Alu',
+        price: 20 
+    },
+    {
+        id: 0,
+        name: 'Alu',
+        price: 20 
+    },
+    {
+        id: 0,
+        name: 'Alu',
+        price: 20 
+    }
+]
+
+console.log( productData );
